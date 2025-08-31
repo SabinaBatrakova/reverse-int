13b9fcda6302a21c9e3ba42bb3e926963f3f6acf
Sabina Batrakova <mms8818@mail.ru>
2025-08-31 17:19:43 +0200

    feat: add desision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const nStr = Math.abs(n).toString();
+  const reverseStr = nStr.split('').reverse().join('');
+  return parseInt(reverseStr, 10);
 };
